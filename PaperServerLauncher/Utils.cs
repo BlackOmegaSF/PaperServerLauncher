97d2220c3027f5e20850e010f56cd7630e9d2d85
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-15 09:22:15 -0800

    Add utility for rounding down to power of 2

---
+++ PaperServerLauncher/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperServerLauncher
+{
+    static class Utils
+    {
+        public static class Constants
+        {
+            public static int UNIT_MODE_MB = 0;
+            public static int UNIT_MODE_GB = 1;
+        }
+
+        public static class Maths
+        {
+            public static UInt64 roundToPowerOf2(double number)
+            {
+                UInt64 power = 1;
+                while (power < number)
+                    power*= 2;
+                return power / 2;
+            }
+        }
+        
+    }
+}
