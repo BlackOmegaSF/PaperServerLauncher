05210b0051ca19e4b5c0d96bcda899b05bb9c852
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-15 09:47:42 -0800

    Add method to get and recommend ram amount

--- PaperServerLauncher/PaperServerLauncher.cs
+++ PaperServerLauncher/PaperServerLauncher.cs
@@ -18,7 +18,12 @@ namespace PaperServerLauncher
         public ServerLauncher()
         {
             InitializeComponent();
-            cbRamUnits.SelectedIndex = 0;
+            cbRamUnits.SelectedIndex = 1;
+
+            //TODO Load and apply settings
+
+            //Get and update RAM
+            updateInstalledRamLabel(cbRamUnits.SelectedIndex);
         }
 
         private void numRAM_KeyPress(object sender, KeyPressEventArgs e)
@@ -34,9 +39,52 @@ namespace PaperServerLauncher
             {
                 ramBytes += (UInt64)ramObject["Capacity"];
             }
-            //TODO do math based on loaded settings
-            //TODO Set label accordingly
+            //DEBUG
+            ramBytes = (UInt64)(16 * Math.Pow(2, 30));
+            //Doo math, since it's the same no matter the units
+            UInt64 recRam;
+            //If you have less than 4GB, recommend none
+            if (ramBytes < (4 * Math.Pow(2, 30)))
+            {
+                recRam = 0;
+            }
+            else if (ramBytes < (20 * Math.Pow(2, 30)))
+            {
+                recRam = (UInt64)(ramBytes - (2 * Math.Pow(2, 30)));
+            }
+            else
+            {
+                recRam = (UInt64)(16 * Math.Pow(2, 30));
+            }
+
+            //TODO format based on loaded settings
+            //Special case for potatoes
+            if (recRam == 0)
+            {
+                lblCurrentRam.Text = "Running the server is not recommended, not enough RAM!";
+                numRAM.Value = 0;
+                return;
+            }
+            UInt64 displayRAM = 0;
+            UInt64 displayRecRam = 0;
+            string ramUnits = "";
+            if (unitMode == Utils.Constants.UNIT_MODE_MB)
+            {
+                ramUnits = "MB";
+                displayRAM = ramBytes / (UInt64)Math.Pow(2, 20);
+                displayRecRam = recRam / (UInt64)Math.Pow(2, 20);
+            } 
+            else if (unitMode == Utils.Constants.UNIT_MODE_GB)
+            {
+                ramUnits = "GB";
+                displayRAM = ramBytes / (UInt64)Math.Pow(2, 30);
+                displayRecRam = recRam / (UInt64)Math.Pow(2, 30);
+            }
+            //Set label accordingly
+            lblCurrentRam.Text = "Recommend " + displayRecRam.ToString() + ramUnits + " of " + displayRAM.ToString() + ramUnits + " installed";
+            numRAM.Value = displayRecRam;
         }
+
 
         //Hide text caret on plugin output text box
         [DllImport("user32.dll")]
@@ -46,10 +94,12 @@ namespace PaperServerLauncher
             HideCaret(textBox1.Handle);
         }
 
+        //Hide text caret on plugin output text box
         private void textBox1_GotFocus(object sender, EventArgs e)
         {
             HideCaret(textBox1.Handle);
         }
+
 
         //Handle browsing for server jar
         private void btnBrowseJar_Click(object sender, EventArgs e)
